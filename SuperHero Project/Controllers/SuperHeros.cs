db96edc51f8ceb77fac219be97126c2cf13f16a9
Penson, Kelly <pensonk86@gmail.com>
2021-02-01 10:47:55 -0800

    Updated Controller added superheros

--- SuperHero Project/Controllers/SuperHeros.cs
+++ SuperHero Project/Controllers/SuperHeros.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SuperHero_Project.Data;
+using SuperHero_Project.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,17 +9,27 @@ using System.Threading.Tasks;
 
 namespace SuperHero_Project.Controllers
 {
-    public class SuperHeros : Controller
+    public class SuperHeroes : Controller
     {
+        private ApplicationDbContext _context; 
+        public SuperHeroes(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        
         // GET: SuperHeros
         public ActionResult Index()
         {
-            return View();
+            var superheros = _context.SuperHeroes.ToList(); // 
+            return View(superheros);
+
         }
 
         // GET: SuperHeros/Details/5
         public ActionResult Details(int id)
         {
+            var superhero = _context.SuperHeroes.Find(id);
             return View();
         }
 
@@ -30,10 +42,12 @@ namespace SuperHero_Project.Controllers
         // POST: SuperHeros/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(SuperHero superhero)
         {
             try
             {
+                _context.SuperHeroes.Add(superhero);
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -45,6 +59,7 @@ namespace SuperHero_Project.Controllers
         // GET: SuperHeros/Edit/5
         public ActionResult Edit(int id)
         {
+            SuperHero superhero = _context.Superheros.FirstDefault(s => s.id == id);
             return View();
         }
 
@@ -55,6 +70,14 @@ namespace SuperHero_Project.Controllers
         {
             try
             {
+                SuperHero superheroToAdd = _context.SuperHeroes.First(s => s.id == id);
+                superheroToAdd.Name = collection["Name"];
+                superheroToAdd.AlterEgo = collection["AlterEgo"];
+                superheroToAdd.PrimarySuperheroAbility = collection["PrimarySuperheroAbility"];
+                superheroToAdd.SecondarySuperHeroAbility = collection["SecondarySuperheroAbility"];
+                superheroToAdd.Catchphrase = collection["Catchphrase"];
+                _context.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -66,7 +89,8 @@ namespace SuperHero_Project.Controllers
         // GET: SuperHeros/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            SuperHero superhero = _context.SuperHeroes.First(s => s.id == id);
+            return View(superhero);
         }
 
         // POST: SuperHeros/Delete/5
@@ -76,6 +100,10 @@ namespace SuperHero_Project.Controllers
         {
             try
             {
+                SuperHero superhero = _context.SuperHeroes.First(s => s.id == id);
+                _context.SuperHeroes.Remove(superhero);
+                _context.SaveChanges();
+               
                 return RedirectToAction(nameof(Index));
             }
             catch
